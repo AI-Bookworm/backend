615db42cc3dad77e8ee6d8a15b9dc0cd42643322
Deven Mallamo <neved1234@gimail.com>
2025-02-23 13:51:54 -0500

    added test api code with stub data

---
+++ api.py
@@ -0,0 +1,9 @@
+'''
+Code which defines the logic of the api.
+'''
+
+def get_book_data(image):
+    '''
+        Main function called by flask to take in an image of a book, and return info about the book.
+    '''
+    pass
--- app.py
+++ app.py
@@ -1,27 +1,45 @@
 from flask import Flask, request, jsonify
+import api
 
 app = Flask(__name__)
 logger = app.logger
 
-@app.route('/upload-image', methods=['POST'])
+@app.route('/api/upload-image', methods=['POST'])
 def upload_image():
-    if 'image' not in request.files:
-        return jsonify({'message': 'No image part in the request'}), 400
+    test_mode = request.args.get('test')
     
-    image = request.files['image']
+    if test_mode == None:
+        # The "real" code will go here.
+        if 'image' not in request.files:
+            return jsonify({'message': 'No image part in the request'}), 400
+        
+        image = request.files['image']
+        return api.get_book_data(image)
     
-    if image.filename == '':
-        return jsonify({'message': 'No image selected for uploading'}), 400
+    if test_mode == 'notfound':
+        return jsonify({'message': 'Test 404 invoked!'}), 404
     
-    # Here you can add code to save the image or process it as needed
+    if test_mode == 'error':
+        return jsonify({'message': 'Test 500 invoked!'}), 500
     
-    print('Image successfully sent')
-    return jsonify({'message': 'Image successfully sent'}), 200
+    if test_mode == 'success':
+        if 'image' not in request.files:
+            return jsonify({'message': 'No image part in the request'}), 400
+    
+        return jsonify({
+            'title': 'Book Title',
+            'subtitle': 'Book Subtitle',
+	        'author':'Author' ,
+	        'rating': 3.5,
+	        'length': '271',
+	        'categories': ['Category 1', 'Category 2'],
+	        'description': 'Book Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+        }), 200
 
-# Use this API to test your local connection
-@app.route('/test', methods=['GET'])
+# Use this API to sanity test your local connection
+@app.route('/', methods=['GET'])
 def test_post():
-    logger.info(f'Received test!')
+    logger.info(f'Connection success!')
     return jsonify({'message' : 'Test Works!'}), 200
 
 if __name__ == '__main__':
